6d203cc34ba0f61586f0f903e88238b778a72a19
Sidney Roberto <sidneyrpsilva@gmail.om>
2025-01-04 10:32:12 -0300

    ✨ feat: Add drizzle orm and configure schema

---
+++ src/drizzle/db.ts
@@ -0,0 +1,9 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+
+import { env } from '@/data/env/server';
+import { neon } from '@neondatabase/serverless';
+
+import * as schema from './schema';
+
+const sql = neon(env.DATABASE_URL);
+export const db = drizzle(sql, { schema, logger: true });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)', '/api(.*)']);
 
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!isPublicRoute(req) && userId == null) {
+    redirectToSignIn();
+  }
+});
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
