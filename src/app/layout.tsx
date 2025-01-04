1112445709e2e6f31778dad042bc512aea07910f
Sidney Roberto <sidneyrpsilva@gmail.om>
2025-01-04 09:08:32 -0300

    ✨ feat: Add Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
 import '@/styles/globals.css';
-import type { Metadata } from 'next';
 
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import { PropsWithChildren } from 'react';
 
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
+import { cn } from '@/lib/utils';
+import { ClerkProvider } from '@clerk/nextjs';
+
+import type { Metadata } from 'next';
+const geistSans = localFont({
+  src: '../styles/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+
+const geistMono = localFont({
+  src: '../styles/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
@@ -15,8 +25,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={cn(
+            geistSans.variable,
+            geistMono.variable,
+            'antialiased font-sans bg-background',
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
